87ee5dadaf833a51ed23a6125cd58ccb52fb5c58
Sviatoslav Romankiv <sviatoslav@source-elements.com>
2023-10-07 18:37:17 +0300

    Initial Commit

---
+++ LinearEquationsSolver/LinearEquationsSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinearEquationsSolverApp.swift
+//  LinearEquationsSolver
+//
+//  Created by Sviatoslav Romankiv on 07.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct LinearEquationsSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
